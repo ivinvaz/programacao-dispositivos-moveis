3933304504a89f5827e2da10af657628c02b74ef
Ivin <vaz.ivin@gmail.com>
2025-03-06 20:07:50 -0300

    Corrigindo o código

--- praticas/pratica02/App.js
+++ praticas/pratica02/App.js
@@ -5,25 +5,30 @@ function AppBar(props) {
     <View style={styles.appBar}>
       <Text style={styles.title}>{props.titulo}</Text>
     </View>
-  );  
+  );
 }
-
 function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.container}>
+    <View style={{flex: 1}}>
         <AppBar titulo="Minha Aplicação React Native" />
-        <View></View>
-      </View>
+    <View style={styles.container}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: "#fff", justifyContent: "center", alignItems: "center"},
-  appBar: {height: 50, backgroundColor: "#333", justifyContent: "center", paddingLeft: 16},
-  title: {fontSize: 20, fontWeight: "bold", color: "#fff"},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appBar: {
+    height: 50,
+    backgroundColor: "#333",
+    justifyContent: "center",
+    paddingLeft: 16,
+  },
+  title: { fontSize: 20, fontWeight: "bold", color: "#fff" },
 });
-
-
 export default App;
