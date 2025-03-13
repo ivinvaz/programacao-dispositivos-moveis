9cbb549524809b7f14ab0a19510b54e863f2a700
Ivin <vaz.ivin@gmail.com>
2025-03-13 20:06:44 -0300

    Aula 03 terminada.

--- aulas/aula03/App.js
+++ aulas/aula03/App.js
@@ -1,17 +1,15 @@
-import {View, Text} from "react-native";
+import Flexbox from "./screens/Flexbox";
+import GaleriaScreen from "./screens/GaleriaScreen";
+import PadraoAndroid from "./screens/PadraoAndroid";
+import PadraoIOS from "./screens/PadraoIOS"
+import PerfilScreen from "./screens/Perfil";
 
 function App(){
   return(
-    //Flex define a área de ocupação, no caso 1 é 100%//Backgorund color define a cor do fundo
-    <View style={{flex: 1,flexDirection: "row", backgroundColor: "#fff"}}>
-      <View style={{flex: 1, justifyContent:"space-evenly", alignItems:"stretch",backgroundColor: "red"}}>
-        <View style={{height: 50, backgroundColor:"black"}}></View>
-        <View style={{height: 50, backgroundColor:"blue"}}></View>
-        <View style={{height: 50, backgroundColor:"gray"}}></View>
-      </View>
-      <View style={{flex: 1,backgroundColor: "green"}}></View>
-    </View>
+    <PerfilScreen style={{flex:1}}>
+
+    </PerfilScreen>
   )
 }
 
-export default App;
+export default PerfilScreen;
