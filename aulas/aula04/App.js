520ce6d3a47ddba38ed5e130d3d211b94551a397
Ivin <vaz.ivin@gmail.com>
2025-03-20 21:46:57 -0300

    Adicionando códigos complementares da aula04.

--- aulas/aula04/App.js
+++ aulas/aula04/App.js
@@ -1,9 +1,11 @@
 import {View,StyleSheet,Text} from 'react-native'
 import RegistrarConta from './screens/RegistrarContas';
 import GaleriaFotos from './screens/GaleriaFotos';
+import DetalhesProduto from './screens/DetalhesProdutos';
+import Conversas from './screens/Conversas';
 
 function App(){
-  return <GaleriaFotos/>
+  return <Conversas/>
 }
 
 export default App;
