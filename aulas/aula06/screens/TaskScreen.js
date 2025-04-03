85fcb2df8d973ec454e38192433526e4676b77ee
Ivin <vaz.ivin@gmail.com>
2025-04-03 20:28:59 -0300

    Adiconando TaskScreen na aula06.

--- aulas/aula06/screens/TaskScreen.js
+++ aulas/aula06/screens/TaskScreen.js
@@ -1,21 +1,32 @@
 import { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import {Appbar, List ,TextInput, FAB, Modal, Button} from 'react-native-paper';
+import { View, StyleSheet, FlatList } from 'react-native';
+import {Appbar, List ,TextInput, FAB, Modal, Button, Divider} from 'react-native-paper';
 
 function TaskScreen(){
     const [exibeModal, setexibeModal] = useState(false);
-    const [concluida, setConcluida] = useState(false);
+    const [tarefa, setTarefa] = useState("");
+    const [tarefas, setTarefas] = useState([]);
     return <View style={styles.container}>
         <Appbar.Header>
             <Appbar.Content title="Tarefas"/>
         </Appbar.Header>
 
-        <List.Item 
-        title="Estudar para a prova" 
-        right={(props) => <List.Icon icon= {concluida 
-            ? "check-circle-outline" 
-            : "circle-outline"}/>}
-        onPress={() => setConcluida(!concluida)}
+        <FlatList
+            data={tarefas}
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => (
+            <>
+                <List.Item 
+                onPress={() => item.concluida = !item.concluida}
+                title={item.nome} 
+                right={(props) => <List.Icon 
+                    {...props}
+                    icon= {item.concluida 
+                    ? "check-circle-outline" 
+                    : "circle-outline"}/>}
+                />
+                <Divider/>
+            </>)}
         />
         <FAB 
         icon="plus" 
@@ -23,15 +34,23 @@ function TaskScreen(){
         onPress={() => setexibeModal(true)}
         />
         <Modal visible={exibeModal}>
-            <TextInput label="Nova Tarefa"/>
-            <Button onPress={() => setexibeModal(false)}>Salvar</Button>
+            <View style={styles.modal}>
+                <TextInput label="Nova Tarefa" onChangeText={(text) => setTarefa(text)}/>
+                <Button onPress={() => {
+                    if (tarefa){
+                        setTarefas([...tarefas, {id: tarefas.length+1, nome: tarefa, concluida: false}])
+                        }
+                    setTarefas("")
+                    setexibeModal(false)
+                    }}>Salvar</Button>
+            </View>
         </Modal>
         
 
     </View>
 }
 
-const styles = StyleSheet.create=({
+const styles = StyleSheet.create({
     container:{
         flex:1,
         padding:16,
@@ -40,6 +59,12 @@ const styles = StyleSheet.create=({
         position: "absolute",
         right: 16,
         bottom: 16,
+    },
+    modal:{
+        padding: 16,
+        backgroundColor: "white",
+        margin: 16,
+        borderRadius: 8,
     },
 
 })
