907a5fdca63d5fb9e1dcb0585d6396688d118fde
Ivin <vaz.ivin@gmail.com>
2025-04-08 21:38:03 -0300

    Adicionando o restante da aula06.

--- aulas/aula06/App.js
+++ aulas/aula06/App.js
@@ -1,9 +1,12 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import TaskScreen from "./screens/TaskScreen";
+import { TaskProvider } from "./contexts/TaskContext";
 
 function App(){
   return <SafeAreaProvider>
+    <TaskProvider>
       <TaskScreen/>
+    </TaskProvider>
   </SafeAreaProvider>
 }
 
--- aulas/aula06/screens/TaskScreen.js
+++ aulas/aula06/screens/TaskScreen.js
@@ -1,72 +1,102 @@
-import { useState } from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
-import {Appbar, List ,TextInput, FAB, Modal, Button, Divider} from 'react-native-paper';
+import { useContext, createContext, useState } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+import {
+  Appbar,
+  List,
+  TextInput,
+  FAB,
+  Modal,
+  Button,
+  Divider,
+  Text,
+} from "react-native-paper";
+import { TaskContext } from "../contexts/TaskContext";
 
-function TaskScreen(){
-    const [exibeModal, setexibeModal] = useState(false);
-    const [tarefa, setTarefa] = useState("");
-    const [tarefas, setTarefas] = useState([]);
-    return <View style={styles.container}>
-        <Appbar.Header>
-            <Appbar.Content title="Tarefas"/>
-        </Appbar.Header>
-
-        <FlatList
-            data={tarefas}
-            keyExtractor={(item) => item.id}
-            renderItem={({item}) => (
-            <>
-                <List.Item 
-                onPress={() => item.concluida = !item.concluida}
-                title={item.nome} 
-                right={(props) => <List.Icon 
-                    {...props}
-                    icon= {item.concluida 
-                    ? "check-circle-outline" 
-                    : "circle-outline"}/>}
+function TaskScreen() {
+  const {selecionar, remover, concluir, adicionar, tarefas} = useContext(TaskContext) 
+  const [tarefa, setTarefa] = useState("");
+  const [exibeModal, setExibeModal] = useState(false);
+  const [exibeAlerta, setExibeAlerta] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Appbar.Header>
+        <Appbar.Content title="Minhas Tarefas" />
+      </Appbar.Header>
+      <FlatList
+        data={tarefas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <>
+            <List.Item
+              onLongPress={() => {
+                selecionar(item.id);
+                setExibeAlerta(true);
+              }}
+              onPress={() => {concluir(item.id);}}
+              title={item.nome}
+              right={(props) => (
+                <List.Icon
+                  {...props}
+                  icon={
+                    item.concluida ? "check-circle-outline" : "circle-outline"
+                  }
                 />
-                <Divider/>
-            </>)}
-        />
-        <FAB 
-        icon="plus" 
-        style={styles.fab}
-        onPress={() => setexibeModal(true)}
-        />
-        <Modal visible={exibeModal}>
-            <View style={styles.modal}>
-                <TextInput label="Nova Tarefa" onChangeText={(text) => setTarefa(text)}/>
-                <Button onPress={() => {
-                    if (tarefa){
-                        setTarefas([...tarefas, {id: tarefas.length+1, nome: tarefa, concluida: false}])
-                        }
-                    setTarefas("")
-                    setexibeModal(false)
-                    }}>Salvar</Button>
-            </View>
-        </Modal>
-        
-
+              )}
+            />
+            <Divider />
+          </>
+        )}
+      />
+      <FAB onPress={() => setExibeModal(true)} icon="plus" style={styles.fab} />
+      <Modal visible={exibeModal}>
+        <View style={styles.modal}>
+          <TextInput
+            label="Nova Tarefa"
+            onChangeText={(text) => setTarefa(text)}
+          />
+          <Button
+            onPress={() => {
+              adicionar(tarefa)
+              setExibeModal(false);
+            }}
+          >
+            Salvar
+          </Button>
+        </View>
+      </Modal>
+      <Modal visible={exibeAlerta}>
+        <View style={styles.modal}>
+          <Text variant="labelLarge">Deseja apagar a tarefa?</Text>
+          <Button
+          onPress={() => setExibeAlerta(false)}>Não</Button>
+          <Button
+          onPress={() => {
+            remover()
+            setExibeAlerta(false);
+            }}>Sim</Button>
+        </View>
+      </Modal>
     </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:16,
-    },
-    fab:{
-        position: "absolute",
-        right: 16,
-        bottom: 16,
-    },
-    modal:{
-        padding: 16,
-        backgroundColor: "white",
-        margin: 16,
-        borderRadius: 8,
-    },
 
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fab: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+  },
+  modal: {
+    backgroundColor: "white",
+    padding: 16,
+    margin: 16,
+    borderRadius: 8,
+  },
+});
+
 
 export default TaskScreen;
