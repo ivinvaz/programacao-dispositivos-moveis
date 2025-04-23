dab48d1d69dbd3dcc5674d43bb05f7b336d59ecc
Ivin <vaz.ivin@gmail.com>
2025-04-22 21:47:03 -0300

    Adicionando aula07

---
+++ aulas/aula07/App.js
@@ -0,0 +1,13 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import Pilha from "./routes/Pilha";
+
+function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Pilha />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
