e3d58e17ef5710607aed7d3b13d324b40c8d86ac
Ivin <vaz.ivin@gmail.com>
2025-05-13 21:25:19 -0300

    Complementando aula08

--- aulas/aula08/screens/CriarConta.js
+++ aulas/aula08/screens/CriarConta.js
@@ -1,47 +1,108 @@
-import { View } from "react-native-web";
-import { TextInput, Button, HelperText} from "react-native-paper";
+import { View, ScrollView } from "react-native-web";
+import { TextInput, Button, HelperText } from "react-native-paper";
 import { useForm, Controller } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function CriarConta() {
-    const {control, handleSubmit, formState:{errors}} = useForm();
+  const schema = Yup.object().shape({
+    nome: Yup.string().required("Nome é obrigatório"),
+    email: Yup.string()
+      .required("E-mail é obrigatório")
+      .email("E=mail é inválido"),
+    senha: Yup.string()
+      .required("Senha é obrigatória")
+      .min(8, "Deve ter 8 catacteres"),
+    confirmaSenha: Yup.string()
+      .oneOf([Yup.ref("senha"), null], "Senha distintas")
+      .required("Confirme a senha"),
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
-    return <View style={{flex:1,padding:16}}>
-        <Controller control={control} name="nome" rules={{required: "Nome é obrigatório"}}
-        render={({field:{value,onChange, onBlur}}) => (
-            <TextInput mode="outlined" label="Nome" autoCapitalize="words" value={value} onChangeText={onChange} error={errors.nome} onBlur={onBlur}/>
-        )}/>
-        <HelperText type="error" visible={errors.nome}>
-            {errors.nome?.message}
-        </HelperText>
+  return <ScrollView>
+    <View style={{ flex: 1, padding: 16 }}>
+      <Controller
+        control={control}
+        name="nome" //
+        render={({ field: { value, onChange, onBlur } }) => (
+          <TextInput
+            mode="outlined"
+            label="Nome"
+            autoCapitalize="words"
+            value={value}
+            onChangeText={onChange}
+            error={errors.nome}
+            onBlur={onBlur}
+          />
+        )}
+      />
+      <HelperText type="error" visible={errors.nome}>
+        {errors.nome?.message}
+      </HelperText>
+      <Controller
+        control={control}
+        name="email" //
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            mode="outlined"
+            onChangeText={onChange}
+            value={value}
+            error={errors.email}
+          />
+        )}
+      />
+      <HelperText type="error" visible={errors.email}>
+        {errors.email?.message}
+      </HelperText>
+      <Controller
+        control={control}
+        name="senha" //
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="Senha"
+            keyboardType="password"
+            secureTextEntry
+            mode="outlined"
+            onChangeText={onChange}
+            value={value}
+            error={errors.senha}
+          />
+        )}
+      />
+      <HelperText type="error" visible={errors.email}>
+        {errors.senha?.message}
+      </HelperText>
+      <Controller
+        control={control}
+        name="confirmaSenha" //
+        render={({ field: { value, onChange } }) => (
+          <TextInput
+            label="Confirmar Senha"
+            keyboardType="password"
+            secureTextEntry
+            mode="outlined"
+            onChangeText={onChange}
+            value={value}
+            error={errors.confirmaSenha}
+          />
+        )}
+      />
+      <HelperText type="error" visible={errors.email}>
+        {errors.confirmaSenha?.message}
+      </HelperText>
+      <Button mode="contained" onPress={handleSubmit(() => alert("ok"))}>
+        Criar
+      </Button>
+      =
+    </View>
+  </ScrollView>;
+}
 
-        <Controller control={control} name="email" rules={{required: "E-mail é obrigatório"}}
-        render={({field: {value,onChange}}) => (
-            <TextInput label="E-mail" keyboardType="email-address" autoCapitalize="none" mode="outlined" onChangeText={onChange} value={value} error={errors.email}/>
-        )}/>
-        <HelperText type="error" visible={errors.email}>
-            {errors.email?.message}
-        </HelperText>
-
-        <Controller control={control} name="senha" rules={{required: "Senha é obrigatória"}}
-        render={({field: {value,onChange}}) => (
-            <TextInput label="Senha" keyboardType="password" secureTextEntry mode="outlined" onChangeText={onChange} value={value} error={errors.senha}/>
-        )}/>
-        <HelperText type="error" visible={errors.email}>
-            {errors.senha?.message}
-        </HelperText>
-
-        <Controller control={control} name="confirmaSenha" rules={{required: "Confirmar senha é obrigatória"}}
-        render={({field: {value,onChange}}) => (
-            <TextInput label="Confirmar Senha" keyboardType="password" secureTextEntry mode="outlined" onChangeText={onChange} value={value} error={errors.confirmaSenha}/>
-        )}/>
-        <HelperText type="error" visible={errors.email}>
-            {errors.confirmaSenha?.message}
-        </HelperText>
-
-        <Button mode="contained" onPress={handleSubmit(()=> alert("ok"))}>
-            Criar
-        </Button>
-    </View>;
-  }
-  
 export default CriarConta;
--- aulas/aula08/screens/RecuperarSenha.js
+++ aulas/aula08/screens/RecuperarSenha.js
@@ -1,32 +1,33 @@
 import { useState } from "react";
 import { View } from "react-native";
 import { TextInput, Button, HelperText } from "react-native-paper";
+import * as Yup from "yup";
 
 function RedefinirSenha() {
+  const schema = Yup.object().shape({
+    email: Yup.string().required().email("E-mail é inválido.")
+  })
   const [email, setEmail] = useState("");
   const [erro, setErro] = useState("");
 
-  const validaEmail = () => {
-    if (!email) {
-      setErro("E-mail é obrigatorio");
-      return false;
+  const validaEmail = async () => {
+    try {
+      await schema.validate({ email })
+      setErro("");
+      return true;
+    } catch (err) {
+      setErro(err.errors);
+      return false
     }
-
-    if (!email.includes("@")) {
-      setErro("E-mail inválido");
-      return false;
-    }
-
-    return true;
   };
 
-  const trataTexto = (texto) => {
+  const trataTexto = async (texto) => {
     setEmail(texto);
-    validaEmail();
+    await validaEmail();
   }
 
-  const trataEnviar = () => {
-    if (!validaEmail()) {
+  const trataEnviar = async () => {
+    if (! await validaEmail()) {
       return;
     }
     setEmail("");
